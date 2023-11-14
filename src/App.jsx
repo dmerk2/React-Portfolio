70e3585bb5c4bdadf8c833ebc1ae4ddbb39e0931
dmerk2 <dan.merkin@gmail.com>
2023-11-13 22:38:22 -0500

    Adding Outlest with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Header from './components/Header'
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
       <Header />
+      <main className="m-3">
+        <Outlet />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
