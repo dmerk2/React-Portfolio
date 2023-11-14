80c6de71fbba58db8c01a955de64867fbef5fb20
dmerk2 <dan.merkin@gmail.com>
2023-11-14 02:33:32 -0500

    Adding regex pattern to test for valid email

---
+++ src/utils/helpers.js
@@ -0,0 +1,7 @@
+const isValidEmail = (email) => {
+  // Regular expression for a simple email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export default isValidEmail;
