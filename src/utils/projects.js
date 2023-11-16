fbeb7e6c9cf2ff47c6f2530e9dab0f00f89bceb6
dmerk2 <dan.merkin@gmail.com>
2023-11-16 13:00:55 -0500

    Modularize code by adding projects seperately

---
+++ src/utils/projects.js
@@ -0,0 +1,58 @@
+import DogFinder from "../assets/dog-finder.png";
+import NowWereCookin from "../assets/recipe_blog.png";
+import ComfyCafe from "../assets/comfy_cafe.png";
+import CodingQuiz from "../assets/coding-quiz.png";
+import TextEditor from "../assets/text-editor.png";
+import WeatherForecast from "../assets/weather-forecast.png";
+
+const projects = [
+  {
+    title: "Now We're Cooking",
+    description:
+      "Explore a secure and user-friendly recipe blogging site with advanced security features, inviting users to share, discover, and engage in a vibrant culinary community.",
+    link: "https://nowwerecooking-494c285649f9.herokuapp.com/",
+    repo: "https://github.com/RobKaiser97/Recipe-Blog",
+    image: NowWereCookin,
+  },
+  {
+    title: "PWA Text Editor",
+    description:
+      "The JATE Text Editor is a powerful web application designed for writing and editing code. It combines a client-server architecture with robust features to enhance your coding experience.",
+    link: "https://evening-sea-02025-d78ee56e0095.herokuapp.com/",
+    repo: "https://github.com/dmerk2/PWA-Text-Editor",
+    image: TextEditor,
+  },
+  {
+    title: "Dog Finder",
+    description:
+      "A platform for individuals interested in adopting dogs, offering valuable information about specific dog breeds to facilitate informed adoption decisions.",
+    link: "https://dmerk2.github.io/Dog-Finder/",
+    repo: "https://github.com/dmerk2/Dog-Finder",
+    image: DogFinder,
+  },
+  {
+    title: "Coding Quiz",
+    description:
+      "Embark on a coding challenge with a timed multiple-choice quiz, and witness your final score recorded on the high scores page.",
+    link: "https://dmerk2.github.io/Coding-Quiz/",
+    repo: "https://github.com/dmerk2/Coding-Quiz",
+    image: CodingQuiz,
+  },
+  {
+    title: "Comfy Cafe",
+    description: "Description",
+    link: "https://dmerk2.github.io/comfy-cafe/",
+    repo: "",
+    image: ComfyCafe,
+  },
+  {
+    title: "Weather Forecast Dashboard",
+    description:
+      "A user-friendly weather dashboard application that enables users to input a city, view current weather conditions, and access a 5-day forecast",
+    link: "https://dmerk2.github.io/Weather-Forecast-Dashboard/",
+    repo: "https://github.com/dmerk2/Weather-Forecast-Dashboard",
+    image: WeatherForecast,
+  },
+];
+
+export default projects;
